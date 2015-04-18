e2d07f0e37f8898c15fd905053687193fa08fec2
Bhavin Darji <darjibhavinr@gmail.com>
2015-04-18 16:33:50 +0530

    LED examples 1 to 12 1. Write a program to glow single LED on and off. 2.write a program to glow all LEDs on and off. 3.write a program to glow alternate LEDs on and off. 4.write a program to glow LEDs in binary counter fashion. 5.write a program to glow LEDs from Left to Right. 6.write a program to glow LEDs from Right to Left. 7.write a program to glow LEDs in Knight Rider Pattern. 8.Write a program to blink Led PD12 with GPIO interrupt and use SysTick Delay with Falling Edge on PA0 Key 9.Write a program to blink Led PD13 with GPIO interrupt and use SysTick delay with Rising Edge on PA0 Key 10.Write a program to blink Led PD14 pattern only once with Rising Edge after that DeInit NVIC on PA0 Key 11.Write a program to glow on board LEDs on and off.
    12.Write a Program to blink Led PD12 using PA0 key.

--- Example Projects/GPIO/LED/Eg11/Inc/doxy_wb.h
+++ Example Projects/GPIO/LED/Eg11/Inc/doxy_wb.h
@@ -2,55 +2,52 @@
 *//**
 * @mainpage
 * @section section1 Introduction:
-*         Having studied this LAB you will able to: \n
-*         -   Understand the GPIO functions \n
+*          Having studied this LAB you will able to: \n
+*         -   Understand the GPIO on board functions \n
 *         -   Study the programs related to the various patterns of LED flashing
 *
-* @section section2 Example1 :
-*    Objective: Write a program to glow single LED on and off.
+* @section section2 Example 11 :
+*    Objective: Write a Program to blink board LEDs.
 *
 * @section section3 Program Description:
-*    This program demonstrates blinking of single LED on ASK-25.
+*    This program demonstrates blinking of on board LEDs.
 *
 * @section section4 Included Files:
 *
 *   | Header Files           	  | Source Files           	  |
-*   | :------------------------:| :------------------------:|
-*   | @ref stm32f4xx_hal_conf.h | @ref stm32f4xx_hal_msp.c  |
-*   | @ref stm32f4xx_it.h 		  | @ref stm32f4xx_it.c	 	    |
-*   | @ref stm32f4_ask25.h   	  | @ref stm32f4_ask25.c   	  |
-*   |                           | @ref main.c               |
-*
+*   | :-------------------------: | :------------------------:|
+*   | @ref stm32f4xx_hal_conf.h   | @ref stm32f4xx_hal_msp.c  |
+*   | @ref stm32f4xx_it.h 		  | @ref stm32f4xx_it.c	      |
+*   | @ref stm32f4_discovery.h    | @ref stm32f4_discovery.c  |
+*   |                             | @ref main.c               |
 *
 *
 
 * \n
 * @section section5 Pin Assignments
 *
-*   | STM32F407 Reference | Device (ASK25) |
-*   | :------------------:| :-------------:|
-*   |  GPIO E.8(P1.26)    | LED1           |
+*   | STM32F407 Reference | Device (On board) |
+*   | :------------------:| :---------------: |
+*   | GPIOD.13            | LED3              |
+*   | GPIOD.12            | LED4              |
+*   | GPIOD.14            | LED5              |
+*   | GPIOD.15            | LED6              |
 *
-* @section section6 Connection
-*   | STM32F407 Reference | Device          |
-*   | :------------------:| :-------------: |
-*   | J6                  | ASK-25 (PL8)    |
+*
 *
 * @section section7 Program Folder Location
-*       <Eg1>
+*          <Eg11>
 *
 *
 * @section section8 Part List
 *   - STM32F4Discovery Board \n
-*   - Flat cable \n
 *   - USB cable \n
 *   - Eclipse IDE \n
 *   - PC \n
-*   - ASK-25 Rev2.0 \n
+*
 
 *
 * @section section9 Hardware Configuration
-*   - Connect ASK 25 to educational practice board using flat cable.
 *   - Connect the board using USB port of PC using USB cable.
 *   - Apply Reset condition by pressing the Reset switch to ensure proper communication.
 *   - Using download tool (STM ST-LINK Utility) download the .hex file developed using available tools.
@@ -59,7 +56,7 @@
 
 *
 * @section section10 Output:
-*    LED1 blinks on ASK25.
+*    LED4, 3, 5, 6 blinks
 *\n
 *\n
 *******************************************************************************/
