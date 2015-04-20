cb35c3517c6a2379ff765c4b5319d5245cd03a2e
Bhavin Darji <darjibhavinr@gmail.com>
2015-04-20 10:18:27 +0530

    BSP Template updated

---
+++ Project_Templates/stm32f4_bsp_template/Drivers/BSP/stm32f4_ask25.c
@@ -0,0 +1,353 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4_ask25.c
+  * @author  Dwijay.Edutech Learning Solutions
+  * @version V1.0
+  * @date    06-April-2015
+  * @brief   This file provides set of firmware functions to manage Leds and
+  *          push-button available on STM32F4-ASK25 Kit from Edutech.
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4_ask25.h"
+
+/** @addtogroup BSP
+  * @{
+  */
+
+/** @addtogroup STM32F4_ASK25
+  * @{
+  */
+
+/** @defgroup STM32F4_ASK25_LOW_LEVEL
+  * @brief This file provides set of firmware functions to manage Leds and push-button
+  *        available on STM32F4-ASK25 Kit from Edutech.
+  * @{
+  */
+
+
+/** @defgroup STM32F4_ASK25_LOW_LEVEL_LED_Functions
+  * @{
+  */
+
+/**
+ * @brief Configures ASK25's LED GPIO.
+ * @param LED: Specifies the Led to be configured.
+ *   This parameter can be one of following parameters:
+ *     @arg ALED1 to ALED8
+ *     @arg ALEDALL
+ * @retval None
+ */
+void ASK25_LED_Init(ALED_TypeDef LED)
+{
+  GPIO_InitTypeDef  GPIO_InitStruct;
+
+  /* Enable the GPIO_ALED Clock */
+
+  __GPIOE_CLK_ENABLE();
+
+  /* Configure the GPIO_ALED pin */
+  GPIO_InitStruct.Pin = LED << 8 ;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOE, LED<<8, GPIO_PIN_SET);
+}
+
+
+/**
+ * @brief Turns selected LED On.
+ * @param LED: Specifies the LED to be set on.
+ *   This parameter can be one of following parameters:
+ *     @arg ALED1 to ALED8
+ *     @arg ALEDALL
+ * @retval None
+ */
+void ASK25_LED_On(ALED_TypeDef LED)
+{
+  HAL_GPIO_WritePin(GPIOE,LED<<8, GPIO_PIN_RESET);
+}
+
+
+/**
+ * @brief Turns selected LED Off.
+ * @param LED: Specifies the Led to be set off.
+ *   This parameter can be one of following parameters:
+ *     @arg ALED1 to ALED8
+ *     @arg ALEDALL
+ * @retval None
+ */
+void ASK25_LED_Off(ALED_TypeDef LED)
+{
+  HAL_GPIO_WritePin(GPIOE,LED<<8, GPIO_PIN_SET);
+}
+
+
+/**
+  * @brief  Toggles the selected LED.
+  * @param  LED: Specifies the Led to be toggled.
+  *   This parameter can be one of following parameters:
+ *     @arg ALED1 to ALED8
+ *     @arg ALEDALL
+  * @retval None
+  */
+void ASK25_LED_Toggle(ALED_TypeDef LED)
+{
+  HAL_GPIO_TogglePin(GPIOE, LED<<8);
+}
+
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32F4_ASK25_LOW_LEVEL_BUTTON_Functions
+  * @{
+  */
+
+/**
+  * @brief  Configures Button GPIO and EXTI Line of ASK25.
+  * @param  Button: Specifies the Button to be configured.
+  *   This parameter should be: ABUTTON_KEY1 to ABUTTON_KEY4 or ABUTTON_ALL
+  * @param  Mode: Specifies Button mode.
+  *   This parameter can be one of following parameters:
+  *     @arg ABUTTON_MODE_GPIO: Button will be used as simple IO
+  *     @arg ABUTTON_MODE_EXTI: Button will be connected to EXTI line with interrupt
+  *                             generation capability (Make sure to ENABLE USE_BUTTON_INT
+  *                             in stm32f4_ask25.h file)
+  *                             Note: Include "stm32f4_ask25.h in stm32f4xx_it.c file"
+  * @retval None
+  */
+void ASK25_PB_Init(AButton_TypeDef Button, AButtonMode_TypeDef Mode)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* Enable the BUTTON Clock */
+  __GPIOE_CLK_ENABLE();
+
+  if (Mode == ABUTTON_MODE_GPIO)
+  {
+    /* Configure Button pin as input */
+    GPIO_InitStruct.Pin = Button;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+  }
+
+  if (Mode == ABUTTON_MODE_EXTI)
+  {
+    /* Configure Button pin as input with External interrupt */
+    GPIO_InitStruct.Pin = Button;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    /* Enable and set Button EXTI Interrupt to the lowest priority */
+    HAL_NVIC_SetPriority(EXTI4_IRQn, 1, 0);
+    HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+
+    HAL_NVIC_SetPriority(EXTI9_5_IRQn, 1, 0);
+    HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+  }
+}
+
+
+/**
+  * @brief  Returns the selected Button state.
+  * @param  Button: Specifies the Button to be checked.
+  *   This parameter should be: ABUTTON_KEY1 to ABUTTON_KEY4 or ABUTTON_ALL
+  * @retval The Button GPIO pin value.
+  */
+uint32_t ASK25_PB_GetState(AButton_TypeDef Button)
+{
+  return HAL_GPIO_ReadPin(GPIOE, Button);
+}
+
+/**
+  * @}
+  */
+
+
+/** @defgroup STM32F4_ASK25_LOW_LEVEL_RELAY_Functions
+  * @{
+  */
+
+/**
+ * @brief Configures RELAY of ASK25
+ * @retval None
+ */
+void ASK25_Relay_Init(void)
+{
+  GPIO_InitTypeDef  GPIO_InitStruct;
+
+  /* Enable the GPIO_Relay Clock */
+
+  __GPIOB_CLK_ENABLE();
+
+  /* Configure the GPIO_Relay pin */
+  GPIO_InitStruct.Pin = RELAY_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+
+  HAL_GPIO_Init(RELAY_PORT, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(RELAY_PORT, RELAY_PIN, GPIO_PIN_SET);
+}
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32F4_ASK25_LOW_LEVEL_DCMOTOR_Functions
+  * @{
+  */
+
+/**
+ * @brief Initialize DC Motor on ASK25
+ * @retval None
+ */
+void ASK25_DCMotor_Init(void)
+{
+  GPIO_InitTypeDef  GPIO_InitStruct;
+
+  /* Enable the DCMotor Clock */
+
+  __GPIOB_CLK_ENABLE();
+
+  /* Configure the GPIO_ALED pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+}
+
+
+/**@defgroup STM32F4_ASK25_7Segment_Functions
+ * @{
+ */
+
+/**
+ * @brief Initialize 7segment on ASK25
+ * @return none
+ */
+void ASK25_7Segment_Init(void)
+{
+  GPIO_InitTypeDef  GPIO_InitStruct;
+
+  /* Enable the 7Segment clock */
+  __GPIOB_CLK_ENABLE();
+  __GPIOE_CLK_ENABLE();
+
+ /* Configure 7 Segment Data pin */
+  GPIO_InitStruct.Pin = 0xFF << 8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /* Enable the 7 Segment selection and gate pin */
+  GPIO_InitStruct.Pin =  GPIO_PIN_4 | GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);// Select first Segment
+  HAL_GPIO_WritePin(GPIOE, 0xFF << 8, GPIO_PIN_SET); // Common anode so 0xFF clears display
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET); // gate signal high
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET); // latch signal on falling edge
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET); // Select second Segment
+  HAL_GPIO_WritePin(GPIOE, 0xFF << 8, GPIO_PIN_SET); // Common anode so 0xFF clears display
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET); // gate signal high
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET); // latch signal on falling edge
+}
+
+
+/**
+ * @brief To display digit on 7 Segment
+ * @param Digit: specifies which digit display on 7 Segment
+ *      This parameter should be any number from 0 to 9
+ * @param Seg: specifies which Segment to be selected
+ *      This parameter should be ASEGMENT1 or ASEGMENT2
+ * @return none
+ */
+void ASK25_7Segment_Display_Digit (uint8_t Digit, uint8_t Seg)
+{
+  uint8_t Number[16]= {ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
+
+  /* Select segment */
+  if(Seg == ASEGMENT1)
+  {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);// Select first Segment
+  }
+  else if(Seg == ASEGMENT2)
+  {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET); // Select second Segment
+  }
+
+  HAL_GPIO_WritePin(GPIOE, 0xFF << 8, GPIO_PIN_SET); //  clears display
+  HAL_GPIO_WritePin(GPIOE, Number[Digit] << 8, GPIO_PIN_RESET);
+
+  /* Latch data */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET); // gate signal high
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET); // latch signal on falling edge
+}
+
+/**
+ * @brief To display 2 digit data using both 7 Segment
+ * @param Number: specifies which digit to display
+ *        can be any digit from 00 to 99
+ * @return none
+ */
+void ASK25_7Segment_Display_Data (uint8_t Number)
+{
+  uint8_t i=0;
+  uint8_t j=TOTAL_SEGMENTS-1;
+  uint8_t Digits[TOTAL_SEGMENTS];
+
+  while(1)
+  {
+    Digits[i] = Number % 10; //Store unit value
+    Display_Digit(Digits[i], TOTAL_SEGMENTS - j);
+    j--;
+    i++;
+    Number = Number / 10; //Trim a unit value
+    if(i >= TOTAL_SEGMENTS)
+    {
+      break;
+    }
+  }
+}
+/**
+  * @}
+  */
+
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/*****************************************END OF FILE******************************************/
---
+++ Project_Templates/stm32f4_bsp_template/Drivers/BSP/stm32f4_discovery_uart.c
@@ -0,0 +1,485 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4_discovery_uart.c
+  * @author  Dwijay.Edutech Learning Solutions
+  * @version V1.0
+  * @date    06-April-2015
+  * @brief   This file provides set of firmware functions to manage UART as an
+  *          alternative to STDIO functions.
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4_discovery_uart.h"
+
+/** @addtogroup BSP
+  * @{
+  */
+
+/** @addtogroup STM32F4_DISCOVERY_UART
+  * @{
+  */
+
+/** @defgroup STM32F4_DISCOVERY_UART
+  * @brief This file provides set of firmware functions to manage UART as an
+  *        alternative to STDIO functions such as printf and scanf.
+  * @{
+  */
+
+
+/** @defgroup STM32F4_DISCOVERY_UART_Exported_Handle
+ * @{
+ */
+UART_HandleTypeDef huart6;
+DMA_HandleTypeDef hdma_usart6_tx;
+DMA_HandleTypeDef hdma_usart6_rx;
+
+uint8_t EscFlag = 0;
+uint8_t UartReady = 0;
+
+/**
+  * @}
+  */
+
+
+/** @defgroup STM32F4_DISCOVERY_UART_Private_FunctionPrototypes
+  * @{
+  */
+
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32F4_DISCOVERY_UART_Private_Functions
+  * @{
+  */
+
+/**
+ * @brief This function configures Pins for UART6
+ * @param huart Uart Handle
+ * @retval  None
+ */
+static void BSP_UART_MspInit(UART_HandleTypeDef* huart)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  if(huart->Instance==USART6)
+  {
+    /* Peripheral clock enable */
+    __USART6_CLK_ENABLE();
+
+    /**USART6 GPIO Configuration
+    PC6     ------> USART6_TX
+    PC7     ------> USART6_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+    GPIO_InitStruct.Alternate = GPIO_AF8_USART6;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+#ifdef DMA_MODE
+    /* Peripheral DMA init*/
+    hdma_usart6_tx.Instance = DMA2_Stream6;
+    hdma_usart6_tx.Init.Channel = DMA_CHANNEL_5;
+    hdma_usart6_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart6_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart6_tx.Init.MemInc = DMA_MINC_DISABLE;
+    hdma_usart6_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart6_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart6_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart6_tx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart6_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    HAL_DMA_Init(&hdma_usart6_tx);
+
+    __HAL_LINKDMA(huart,hdmatx,hdma_usart6_tx);
+
+    hdma_usart6_rx.Instance = DMA2_Stream1;
+    hdma_usart6_rx.Init.Channel = DMA_CHANNEL_5;
+    hdma_usart6_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_usart6_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart6_rx.Init.MemInc = DMA_MINC_DISABLE;
+    hdma_usart6_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart6_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart6_rx.Init.Mode = DMA_CIRCULAR;
+    hdma_usart6_rx.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_usart6_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    HAL_DMA_Init(&hdma_usart6_rx);
+
+    __HAL_LINKDMA(huart,hdmarx,hdma_usart6_rx);
+#endif
+
+#ifndef POLLING_MODE
+    /* Peripheral interrupt init*/
+    HAL_NVIC_SetPriority(USART6_IRQn, 3, 0);
+    HAL_NVIC_EnableIRQ(USART6_IRQn);
+#endif
+  }
+}
+
+
+static HAL_StatusTypeDef UART_Transmit(uint8_t *data_buf, uint16_t size, uint32_t timeout)
+{
+  HAL_StatusTypeDef status=HAL_ERROR;
+  uint8_t i;
+
+#ifdef POLLING_MODE
+  status = HAL_UART_Transmit(&huart6, data_buf, size, timeout);
+#endif
+
+#ifdef INTERRUPT_MODE
+  status = HAL_UART_Transmit_IT(&huart6, data_buf, size);
+  HAL_Delay(1);
+#endif
+
+#ifdef DMA_MODE
+  status = HAL_UART_Transmit_DMA(&huart6, data_buf, size);
+  HAL_Delay(1);
+#endif
+
+  return (status);
+}
+
+
+static HAL_StatusTypeDef UART_Receive(uint8_t *data_buf, uint16_t size, uint32_t timeout)
+{
+  HAL_StatusTypeDef status=HAL_ERROR;
+
+  EscFlag = 0;                    /* clear EscapeFlag */
+
+#ifdef POLLING_MODE
+  status = HAL_UART_Receive(&huart6, data_buf, size, timeout);
+#endif
+
+#ifdef INTERRUPT_MODE
+  HAL_UART_Receive_IT(&huart6, data_buf, size);
+  if(UartReady == 1)
+  {
+    UartReady = 0;
+    status = HAL_OK;
+  }
+#endif
+
+#ifdef DMA_MODE
+  HAL_UART_Receive_DMA(&huart6, data_buf, size);
+  if(UartReady == 1)
+  {
+    UartReady = 0;
+    status = HAL_OK;
+  }
+#endif
+
+  return (status);
+}
+
+
+/**
+  * @}
+  */
+
+/** @defgroup STM32F4_DISCOVERY_UART_Functions
+  * @{
+  */
+
+/**
+ * @brief This function initializes UART6 with desired baudrate
+ * @param Baudrate    baudrate value
+ * @retval None
+ */
+void BSP_UART_Init(uint32_t Baudrate)
+{
+#ifdef DMA_MODE
+  /* DMA controller clock enable */
+  __DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  HAL_NVIC_SetPriority(DMA2_Stream6_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream6_IRQn);
+  HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
+#endif
+
+  __GPIOC_CLK_ENABLE();
+
+  huart6.Instance = USART6;
+  huart6.Init.BaudRate = Baudrate;
+  huart6.Init.WordLength = UART_WORDLENGTH_8B;
+  huart6.Init.StopBits = UART_STOPBITS_1;
+  huart6.Init.Parity = UART_PARITY_NONE;
+  huart6.Init.Mode = UART_MODE_TX_RX;
+  huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+
+  BSP_UART_MspInit(&huart6);
+  HAL_UART_Init(&huart6);
+}
+
+
+/**
+ * @brief This function is to De-Initilized UART6
+ * @param huart Uart Handle
+ * @retval  None
+ */
+void BSP_UART_MspDeInit(UART_HandleTypeDef* huart)
+{
+  if(huart->Instance==USART6)
+  {
+    /* Peripheral clock disable */
+    __USART6_CLK_DISABLE();
+
+    /**USART6 GPIO Configuration
+    PC6     ------> USART6_TX
+    PC7     ------> USART6_RX
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_6|GPIO_PIN_7);
+
+#ifdef DMA_MODE
+    /* Peripheral DMA DeInit*/
+    HAL_DMA_DeInit(huart->hdmatx);
+    HAL_DMA_DeInit(huart->hdmarx);
+#endif
+
+#ifndef POLLING_MODE
+    /* Peripheral interrupt Deinit*/
+    HAL_NVIC_DisableIRQ(USART6_IRQn);
+#endif
+  }
+}
+
+
+uint8_t getche(TRANSFER_BLOCK_Type mode)
+{
+  uint8_t key[1];
+  HAL_StatusTypeDef status=HAL_ERROR;
+
+  if(mode == NONE_BLOCKING)
+  {
+    status = UART_Receive(key, 1, 1);
+  }
+  else if(mode == BLOCKING)
+  {
+    while(status != HAL_OK)
+    {
+      status = UART_Receive(key, 1, 1);
+    }
+  }
+
+  /* Got some data */
+  if(status == HAL_OK)
+  {
+    if (key[0] == In_CR)
+    {
+      return(key[0]);
+    }
+    else if (key[0] == In_DELETE || key[0] == In_BACKSPACE)
+    {
+      return(key[0]);
+    }
+    else if (key[0] == In_ESC)
+    {
+      EscFlag = 1;
+      return (0);
+    }
+    else if (key[0] >= ' ')
+    {
+      return (key[0]);
+    }
+    else
+    {
+      UART_Transmit(&key[0], 1, MAX_TIMEOUT);
+    }
+  }
+  return(0);
+}
+
+
+uint8_t get_line(int8_t s[], uint8_t lim)
+{
+  int8_t kb;                 /* input character */
+
+  uint8_t pointer=0;          /* Pointer in buffer */
+  uint8_t count=0;            /* character count */
+  uint8_t temp;
+
+  while(1)
+  {
+    kb = getche(BLOCKING);
+
+    if (kb == In_ESC)                /* if ESCAPE pressed then exit */
+    {
+      return (kb);
+    }
+
+    else if (kb == In_CR)            /* CARRIAGE return ? */
+    {
+      s[pointer] = '\0';             /* put null char on last position */
+      break;                         /* yes, exit from this loop */
+    }
+
+    else if(kb == In_DELETE || kb == In_BACKSPACE)
+    {
+      if(pointer==0)                 /* any characters entered */
+      {
+        continue;                    /* no, so get another character */
+      }
+
+      temp = Out_BACKSPACE;
+      UART_Transmit(&temp, 1, MAX_TIMEOUT);   /* go back one position */
+      temp = Out_SPACE;
+      UART_Transmit(&temp, 1, MAX_TIMEOUT);   /* erase char */
+      temp = Out_BACKSPACE;
+      UART_Transmit(&temp, 1, MAX_TIMEOUT);   /* go back one more position */
+
+      pointer--;                     /* decrement pointer */
+      count--;                       /* decrement character count */
+
+      continue;
+    }
+
+    else if(pointer < lim)
+    {
+      s[pointer] = kb;               /* save character and increment pointer */
+      pointer++;
+      count++;                       /* increment count */
+      UART_Transmit(&kb, 1, MAX_TIMEOUT);    /* echo character */
+
+      continue;                      /* and get some more */
+    }
+    else
+    {
+      temp = In_BELL;
+      UART_Transmit(&temp, 1, MAX_TIMEOUT);   /* ring the bell */
+    }
+  }
+  return(count);
+}
+
+
+HAL_StatusTypeDef uprintf(const char *format, ...)
+{
+  uint8_t hex[]= "0123456789ABCDEF";
+  uint32_t width_dec[10] = { 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000};
+  uint32_t width_hex[10] = { 0x1, 0x10, 0x100, 0x1000, 0x10000, 0x100000, 0x1000000, 0x10000000};
+  uint32_t temp;
+
+  int8_t format_flag, fill_char;
+  uint32_t u_val, div_val;
+  uint16_t base;
+
+  int8_t *ptr;
+
+  va_list ap;
+  va_start(ap, format);
+
+  for(;;)
+  {
+    while((format_flag = *format++) != '%')      /* until full format string read */
+    {
+      if(!format_flag)
+      {                        /* until '%' or '\0' */
+        return (HAL_OK);
+      }
+      UART_Transmit(&format_flag, 1, MAX_TIMEOUT);
+    }
+
+    switch(format_flag = *format++)
+    {
+      case 'c':
+        format_flag = va_arg(ap, int32_t);
+        UART_Transmit(&format_flag, 1, MAX_TIMEOUT);
+
+        continue;
+
+      default:
+        UART_Transmit(&format_flag, 1, MAX_TIMEOUT);
+
+        continue;
+
+      case 'b':
+        format_flag = va_arg(ap,int32_t);
+        UART_Transmit(&(hex[(uint16_t)format_flag >> 4]), 1, MAX_TIMEOUT);
+        UART_Transmit(&(hex[(uint16_t)format_flag & 0x0F]), 1, MAX_TIMEOUT);
+
+        continue;
+
+      case 's':
+        ptr = va_arg(ap, int8_t *);
+        while(*ptr)
+        {
+          UART_Transmit(&(*ptr++), 1, MAX_TIMEOUT);
+        }
+
+        continue;
+
+      case 'u':
+        base = 16;
+        div_val = 0x100000;
+        u_val = va_arg(ap, uint32_t);
+        do
+        {
+          UART_Transmit(&(hex[u_val/div_val]), 1, MAX_TIMEOUT);
+          u_val %= div_val;
+          div_val /= base;
+        }while(div_val);
+
+        continue;
+
+      case 'd':
+        base = 10;
+        fill_char = *format++;
+        format_flag = ( *format++ ) - '1';
+        div_val = width_dec[format_flag];
+        u_val = va_arg(ap,int32_t);
+
+        if(((int)u_val) < 0)
+        {
+          u_val = - u_val;    /* applied to unsigned type, result still unsigned */
+          temp = '-';
+          UART_Transmit(&temp, 1, MAX_TIMEOUT);
+        }
+
+        goto  CONVERSION_LOOP;
+
+      case 'x':
+        base = 16;
+        fill_char = *format++;
+        format_flag = (*format++) - '1';
+        div_val = width_hex[format_flag];
+        u_val = va_arg(ap, int32_t);
+
+        CONVERSION_LOOP:
+        while(div_val > 1 && div_val > u_val)
+        {
+          div_val /= base;
+          UART_Transmit(&fill_char, 1, MAX_TIMEOUT);
+        }
+
+        do
+        {
+          UART_Transmit(&(hex[u_val/div_val]), 1, MAX_TIMEOUT);
+          u_val %= div_val;
+          div_val /= base;
+        }while(div_val);
+    }/* end of switch statement */
+  }
+  return(HAL_OK);
+}
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/*****************************************END OF FILE******************************************/
