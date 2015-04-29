dbff3bd2c4c82b72c64499c45cbf450b215d2327
Bhavin Darji <darjibhavinr@gmail.com>
2015-04-29 16:49:33 +0530

    UART examples 	polling 1 to 5 	interrupt 1 to 5 	dma 1 to 4

---
+++ Example Projects/UART/DMA/Eg2/Inc/doxy_wb.h
@@ -0,0 +1,63 @@
+/*******************************************************************************
+*//**
+* @mainpage
+* @section section1 Introduction:
+*         Having studied this LAB you will able to: \n
+*         -   Understand the UART functions \n
+*         -   Study the programs related to UART
+*
+* @section section2 Example2 :
+*    Objective: Write a program to transmit character string using UART6 (DMA).
+*
+* @section section3 Program Description:
+*    This program transmits character string using UART6
+*
+* @section section4 Included Files:
+*
+*   | Header Files           	      | Source Files           	     |
+*   | :----------------------------:| :---------------------------:|
+*   | @ref stm32f4xx_hal_conf.h     | @ref stm32f4xx_hal_msp.c     |
+*   | @ref stm32f4xx_it.h 		      | @ref stm32f4xx_it.c	 	       |
+*   | @ref stm32f4_discovery_uart.h | @ref stm32f4_discovery_uart.c|
+*   |                               | @ref main.c                  |
+*
+* \n
+* @section section5 Pin Assignments
+*
+*   | STM32F407 Reference | Interface      |
+*   | :------------------:| :-------------:|
+*   | P2.47 PC.6          | USART6_TX      |
+*   | P2.48 PC.7          | USART6_RX      |
+*
+* @section section6 Connection
+*   | STM32F407 Reference |
+*   |:-------------------:|
+*   | COM1                |
+*
+* @section section7 Program Folder Location
+*       <Eg2>
+*
+*
+* @section section8 Part List
+*   - STM32F4Discovery Board \n
+*   - USB cable \n
+*   - Serial cable\n
+*   - Eclipse IDE \n
+*   - PC \n
+*   - Hyper terminal\n
+
+*
+* @section section9 Hardware Configuration
+*   - Connect the board using USB port of PC using USB cable.
+*   - Connect serial cable to pc and other end to COM1 on board using UART6.
+*   - Apply Reset condition by pressing the Reset switch to ensure proper communication.
+*   - Using download tool (STM ST-LINK Utility) download the .hex file developed using available tools.
+*   - Reset the board.
+*   - Observe the Output.
+
+*
+* @section section10 Output:
+*    Character string will be displayed on hyper terminal utility.
+*\n
+*\n
+*******************************************************************************/
