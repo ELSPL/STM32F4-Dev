777d707be22121184192b11a28279bf423082ee4
Dwijay Bane <dwijaybane2@gmail.com>
2015-06-03 13:11:37 +0530

    Absolute macro added

--- Project_Templates/stm32f4_bsp_template/Inc/stm32f4_global.h
+++ Project_Templates/stm32f4_bsp_template/Inc/stm32f4_global.h
@@ -136,7 +136,7 @@
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
-
+#define ABS(x)    ((x < 0) ? (-x) : x)
 
 /********************************************************************//**
 * @brief Bit-Banding Macro Functions
