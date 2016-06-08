c1fedf9a49749c12f015b4d5f948c8deb97c8e97
Dwijay Bane <dwijaybane2@gmail.com>
2016-06-08 18:36:21 +0530

    Idle should yield option updated with task notification feature

--- Project_Templates/stm32f4_freertos_template/Inc/FreeRTOSConfig.h
+++ Project_Templates/stm32f4_freertos_template/Inc/FreeRTOSConfig.h
@@ -98,6 +98,7 @@
 #endif
 
 #define configUSE_PREEMPTION                     1
+#define configUSE_TIME_SLICING                   1
 #define configUSE_IDLE_HOOK                      1
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
@@ -108,6 +109,8 @@
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
+#define configIDLE_SHOULD_YIELD                  1
+#define configUSE_TASK_NOTIFICATIONS             1
 #define configUSE_MUTEXES                        1
 #define configQUEUE_REGISTRY_SIZE                8
 #define configCHECK_FOR_STACK_OVERFLOW           2
